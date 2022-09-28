d4588c8ed95e56e6f2cbfdc028c5bd5d848bede3
Jahongir <exemple@gmail.com>
2022-09-28 18:13:57 +0300

    first

---
+++ script.js
@@ -0,0 +1,23 @@
+const noRefresh2= document.querySelector(".btn-about");
+const noRefresh3 = document.querySelector('.btn-skills');
+
+
+  noRefresh2.addEventListener("click", (e) => {
+    e.preventDefault()
+  })
+
+  noRefresh3.addEventListener("click", (e) => {
+    e.preventDefault()
+  })
+
+
+
+const name = document.querySelector("#name")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const message = document.querySelector("#message")
+
+const postData = () => {
+    const res fe
+}
+
